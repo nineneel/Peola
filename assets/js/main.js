8977d989e86dae307aedf6a358cc816c5e1d1047
unknown <sjrnl27@gmail.com>
2022-02-13 19:05:34 +0700

    Recode

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,5 +14,31 @@ function menuShow(id, className) {
 const toggleMenu = document.querySelector(".header__toggle");
 
 toggleMenu.addEventListener("click", () => {
-    menuShow("navbar", "show-menu");
+    menuShow("sidenav", "show-menu");
+});
+
+/*===== MODAL SHOW =====*/
+function modalShow(id, className) {
+    const modal = document.getElementById(id);
+
+    if (modal.classList.contains(className)) {
+        modal.classList.remove(className);
+    } else {
+        modal.classList.add(className);
+    }
+}
+
+const todoItems = document.querySelectorAll(".task__item");
+
+todoItems.forEach((todoItem) => {
+    todoItem.addEventListener("click", () => {
+        menuShow("modal", "show-modal");
+    });
+});
+
+// close modal button
+const modalCloseBtn = document.querySelector(".modal__close");
+
+modalCloseBtn.addEventListener("click", () => {
+    menuShow("modal", "show-modal");
 });
